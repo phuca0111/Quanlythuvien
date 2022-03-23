2ed10a7d1388cef21a224687e2849ecccedf71c3
Tấn phúc Trần phan <tanphucb01@gmail.com>
2022-03-24 03:44:50 +0700

    Models

--- qltv/qltv/Models/Sinhvien.cs
+++ qltv/qltv/Models/Sinhvien.cs
@@ -13,13 +13,14 @@ namespace qltv.Models
         }
 
         public int SinhvienId { get; set; }
-        public int Masosinhvien { get; set; }
+        public string Masosinhvien { get; set; }
         public string Tensinhvien { get; set; }
         public string Diachi { get; set; }
         public string Lop { get; set; }
         public string Email { get; set; }
         public string Matkhau { get; set; }
         public string SoCmnd { get; set; }
+        public DateTime? Ngaysinh { get; set; }
 
         public virtual ICollection<Muon> Muons { get; set; }
     }
